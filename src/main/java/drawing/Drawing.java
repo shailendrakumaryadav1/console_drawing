ba748095dcb887401e621887719da47633a13f80
shailendrakumaryadav1 <shailendra220893@gmail.com>
2018-03-11 15:53:22 +0800

    README.md file added.

---
+++ src/main/java/drawing/Drawing.java
@@ -0,0 +1,12 @@
+package drawing;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
--- src/test/java/drawing/DrawingTest.java
+++ src/test/java/drawing/DrawingTest.java
@@ -1,4 +1,4 @@
-package com;
+package drawing;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
